a3b822e427edfe50a5cd1b4e4d5962467703c9c4
Roberto <robertogianocca@outlook.com>
2025-12-10 13:24:12 +0100

    Nuovo reset.

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="bg-slate-900 text-slate-200 p-10 h-dvh">
       <div className="size-100 rounded-2xl mx-auto p-10 bg-slate-600">
         <h1>SONO IL COMMIT N.1</h1>
+        <h1>RIPARTIAMO DAL RESET</h1>
       </div>
     </div>
   );
