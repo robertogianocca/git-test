3e7ce09743f93ea97e200d03546b1a02ee763ba8
Roberto <robertogianocca@outlook.com>
2025-12-10 13:27:34 +0100

    Commit n02

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
       <div className="size-100 rounded-2xl mx-auto p-10 bg-slate-600">
         <h1>SONO IL COMMIT N.1</h1>
         <h1>RIPARTIAMO DAL RESET</h1>
+        <h1>SONO IL COMMIT N.2</h1>
       </div>
     </div>
   );
